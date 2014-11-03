9cc3a919c047852ce8f1c2a63dfc7ea0bb7682c6
XiaoxiongZheng <zhengxx012@gmail.com>
2014-11-03 12:25:22 +0800

    plus one

---
+++ LeetCode/LargestRectangleInHistogram/Solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+
+public:
+	int largestRectangleArea(vector<int> &height)
+	{
+		if (height.size() == 0)
+			return 0;
+
+	}
+};
